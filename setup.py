d811d21229f194751f86bcd74913807a22e517a3
Devashish Tripathi <devashishtripathi697@gmail.com>
2024-02-14 01:46:46 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smcpp_pipeline",
+    version="0.1.0",
+    author="Devashish Tripathi",
+    author_email="devashishtripathi697@gmail.com",
+    description="A python pipeline for implementing smcpp package to decipher demographic history of populations",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Devashish13/smcpp_pipeline",
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.18.5",
+        "pandas>=1.0.5",
+        # Add any other package dependencies you need
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
