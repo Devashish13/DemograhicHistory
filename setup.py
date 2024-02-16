1ff7cfb426c2a11446e012e86fcc84b468e353c7
Devashish Tripathi <devashishtripathi697@gmail.com>
2024-02-17 00:40:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="smcpp_pipeline",
-    version="0.2.0",
+    version="0.3.0",
     author="Devashish Tripathi",
     author_email="devashishtripathi697@gmail.com",
     description="A python pipeline for implementing smcpp package to decipher demographic history of populations",
